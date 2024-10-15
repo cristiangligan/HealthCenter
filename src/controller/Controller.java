884144938c063134ac359e24b219d71cefd2a5c7
cristiangligan <cristian.gligan@gmail.com>
2024-10-15 16:54:41 +0200

    UpcomingAppointmentsScreen class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,9 +3,10 @@ package controller;
 import view.DoctorsScreen;
 import view.LogInScreen;
 import view.PatientsScreen;
+import view.UpcomingAppointmentsScreen;
 
 public class Controller {
     public static void main(String[] args) {
-        PatientsScreen patientsScreen = new PatientsScreen(new Controller());
+        UpcomingAppointmentsScreen upcomingAppointmentsScreen = new UpcomingAppointmentsScreen(new Controller());
     }
 }
---
+++ src/view/UpcomingAppointmentsScreen.java
@@ -0,0 +1,44 @@
+package view;
+
+import controller.Controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UpcomingAppointmentsScreen extends JFrame {
+    private Controller controller;
+    private JPanel mainPnl = new JPanel();
+    private JLabel titleLbl = new JLabel("Upcoming appointments");
+    private JButton backBtn = new JButton("Back");
+    private JList upcomingAppointmentsLst = new JList();
+
+    public UpcomingAppointmentsScreen(Controller controller) {
+        this.controller = controller;
+        this.setTitle("Health Center");
+        this.setContentPane(mainPnl);
+        SpringLayout springLayout = new SpringLayout();
+        mainPnl.setLayout(springLayout);
+
+        titleLbl.setFont(new Font(null, Font.PLAIN, 32));
+        mainPnl.add(titleLbl);
+        springLayout.putConstraint(SpringLayout.NORTH, titleLbl, 10, SpringLayout.NORTH, mainPnl);
+        springLayout.putConstraint(SpringLayout.WEST, titleLbl, 40, SpringLayout.WEST, mainPnl);
+
+        mainPnl.add(upcomingAppointmentsLst);
+        springLayout.putConstraint(SpringLayout.NORTH, upcomingAppointmentsLst, 100, SpringLayout.NORTH, mainPnl);
+        springLayout.putConstraint(SpringLayout.WEST, upcomingAppointmentsLst, 40, SpringLayout.WEST, mainPnl);
+        springLayout.putConstraint(SpringLayout.EAST, upcomingAppointmentsLst, -40, SpringLayout.EAST, mainPnl);
+        springLayout.putConstraint(SpringLayout.SOUTH, upcomingAppointmentsLst, -60, SpringLayout.SOUTH, mainPnl);
+
+        mainPnl.add(backBtn);
+        springLayout.putConstraint(SpringLayout.NORTH, backBtn, 10, SpringLayout.SOUTH, upcomingAppointmentsLst);
+        springLayout.putConstraint(SpringLayout.WEST, backBtn, 0, SpringLayout.WEST, upcomingAppointmentsLst);
+
+        this.pack();
+        this.setVisible(true);
+        this.setSize(new Dimension(600, 400));
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
