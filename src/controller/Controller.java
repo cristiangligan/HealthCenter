97572e69a1779618b3b64b1ec5c6a37dea65ffc1
cristiangligan <cristian.gligan@gmail.com>
2024-10-30 10:19:03 +0100

    Rename class
    DoctorScreen -> DoctorNewEditScreen

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,14 @@ import view.*;
 
 public class Controller {
     public static void main(String[] args) {
+        ChooseBookDoctorScreen chooseBookDoctorScreen = new ChooseBookDoctorScreen(new Controller());
+        DiagnosisScreen diagnosisScreen = new DiagnosisScreen(new Controller());
+        DoctorNewEditScreen doctorNewEditScreen = new DoctorNewEditScreen(new Controller());
+        DoctorsScreen doctorsScreen = new DoctorsScreen(new Controller());
+        LogInScreen logInScreen = new LogInScreen(new Controller());
+        MedicalRecordsScreen medicalRecordsScreen = new MedicalRecordsScreen(new Controller());
+        PatientsScreen patientsScreen = new PatientsScreen(new Controller());
+        UpcomingAppointmentsScreen upcomingAppointmentsScreen = new UpcomingAppointmentsScreen(new Controller());
         ScheduleScreen scheduleScreen = new ScheduleScreen(new Controller());
     }
 }
