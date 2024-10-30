5be7bb1b8aa5d63f06d687f9da24fe98dd08fabd
isabellakatz <isabella.katz@hotmail.com>
2024-10-30 10:06:02 +0100

    Added remaining screen classes

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,10 @@ import view.*;
 
 public class Controller {
     public static void main(String[] args) {
+        LogInScreen logInScreen = new LogInScreen(new Controller());
+        AdminLogIn adminLogIn = new AdminLogIn(new Controller());
+        DoctorLogIn doctorLogIn = new DoctorLogIn(new Controller());
+        PatientLogIn patientLogIn = new PatientLogIn(new Controller());
         ScheduleScreen scheduleScreen = new ScheduleScreen(new Controller());
     }
 }
