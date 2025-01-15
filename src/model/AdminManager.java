6839ed334b205d5a00b1f30c1fcca451634664df
cristiangligan <70061069+cristiangligan@users.noreply.github.com>
2025-01-15 14:54:03 +0100

    Get doctors in admin manager property change (#78)

--- src/model/AdminManager.java
+++ src/model/AdminManager.java
@@ -149,7 +149,8 @@ public class AdminManager {
                 statement.setString(4, phone);
                 statement.setInt(5, specialization.getId());
                 int rowCount = statement.executeUpdate();
-                propertyChangeSupport.firePropertyChange(UPDATE_DOCTORS_LIST, null, getSpecializations());
+                propertyChangeSupport.firePropertyChange(UPDATE_DOCTORS_LIST, null, getDoctors());
+                //här
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
